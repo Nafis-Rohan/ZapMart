d1cb5624946d675ca54d014c19ae0e2a5ac5dda3
NafisRohan12 <nafisislamrohan@gmail.com>
2026-09-21 21:11:23 +0600

    chore: initial Spring Boot project setup (ZapMart)

---
+++ src/main/java/com/nafis/ZapMart/ZapMartApplication.java
@@ -0,0 +1,13 @@
+package com.nafis.ZapMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nafis/ZapMart/ZapMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nafis.ZapMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
